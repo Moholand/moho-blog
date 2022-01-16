8957218748092ec6250246daaa09f1f3cbc55381
Moholand <moho.akbarzadeh@gmail.com>
2022-01-16 19:49:16 +0330

    add category_id to posts table

--- database/migrations/2022_01_16_140228_create_posts_table.php
+++ database/migrations/2022_01_16_140228_create_posts_table.php
@@ -18,6 +18,7 @@ class CreatePostsTable extends Migration
             $table->string('title');
             $table->text('content');
             $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('category_id');
             $table->string('image')->nullable();
             $table->timestamps();
         });
