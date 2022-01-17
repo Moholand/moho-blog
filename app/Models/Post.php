34b86911452855af5965ab7db3b8583cab418cc3
Moholand <moho.akbarzadeh@gmail.com>
2022-01-17 10:05:58 +0330

    add relations

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,12 +2,41 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\Like;
+use App\Models\User;
+use App\Models\Comment;
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title'];
+    protected $fillable = [
+        'title', 
+        'content',
+        'image',
+        'category_id'
+    ];
+
+    public function author() 
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function category() 
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,9 @@
 
 namespace App\Models;
 
+use App\Models\Like;
+use App\Models\Post;
+use App\Models\Comment;
 use Laravel\Sanctum\HasApiTokens;
 use Tymon\JWTAuth\Contracts\JWTSubject;
 use Illuminate\Notifications\Notifiable;
@@ -61,5 +64,20 @@ class User extends Authenticatable implements JWTSubject
     public function getJWTCustomClaims()
     {
         return [];
+    }
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
+
+    public function comments()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    public function likes()
+    {
+        return $this->hasMany(Like::class);
     }
 }
